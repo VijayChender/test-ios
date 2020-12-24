572fe78e353f87a19d6d079199a8ae1f52baef51
Santhosh Kumar <santhosh.r.kumar@centurylink.com>
2020-12-24 11:49:38 +0530

    gemfiles added

--- fastlane-plugin-beta_release/lib/fastlane/plugin/beta_release/version.rb
+++
@@ -1,5 +0,0 @@
-module Fastlane
-  module BetaRelease
-    VERSION = "0.1.0"
-  end
-end
--- fastlane-plugin-beta_release/spec/beta_release_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::BetaReleaseAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The beta_release plugin is working!")
-
-      Fastlane::Actions::BetaReleaseAction.run(nil)
-    end
-  end
-end
