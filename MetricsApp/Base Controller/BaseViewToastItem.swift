50a192011128e564f758e7a1d479308b955b36e3
Santhosh Kumar <santhosh.r.kumar@centurylink.com>
2020-12-29 01:02:40 +0530

    m

--- MetricsApp/Base Controller/BaseViewToastItem.swift
+++ MetricsApp/Base Controller/BaseViewToastItem.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 extension BaseViewController{
+    
     func setToastPopup(){
         popToastLabel = UILabel(frame: CGRect(x: offset, y: -50, width: screenWidth - 2 * offset, height: 30))
         popToastLabel.backgroundColor = UIColor(red: 223/255, green: 98/255, blue: 90/255, alpha: 1.0)
