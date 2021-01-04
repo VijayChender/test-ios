3db736279576ea26d5b68519763832c977bf4198
Santhosh Kumar <santhosh.r.kumar@centurylink.com>
2021-01-04 16:07:51 +0530

    removed the error

--- MetricsAppTests/MetricsAppTests.swift
+++ MetricsAppTests/MetricsAppTests.swift
@@ -21,8 +21,6 @@ class MetricsAppTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertNotNil("")
-        l
     }
 
     func testPerformanceExample() throws {
